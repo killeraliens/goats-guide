750550abbe3f5c82cb9c61204b035c49b0dd79d1
alexandra campbell <killeraliens@outlook.com>
2019-03-10 11:31:38 +0000

    events index

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
-  belongs_to :venue
+
+  def date_format
+    self.date.strftime('%a, %m %d, %Y')
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.create!(title: "Graveland Fest", description: "Hyperdontia, Spectral Voice, Asphyx, Necrowretch, Grave", date: "#{Date.new(2019,6,22)} - #{Date.new(2019,6,24)}", time: "doors at 6pm")
+Event.create!(title: "Mortuous", description: "Mortuous helps Tankcrimes and a ton of other sick label-mates take over the Oakland Metro tonight! This is their first show in five months. The full Carbonized Distro will also be available.", date: "#{Date.new(2019,9,11)}", time: "9pm")
