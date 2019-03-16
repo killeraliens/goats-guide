71e7bbe8ced7fd7022fcf217eaf9df67fd4cb7d3
alexandra campbell <killeraliens@outlook.com>
2019-03-16 18:57:18 +0000

    venue event validation changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,16 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :venue
   has_many :saved_events
-  #validates :title, :date, presence: true
+  validates :title, :date, presence: true
   validates :title, :description, case_sensitive: false
   validates :date, uniqueness: { scope: :venue }
 
   def date_format
     self.date.strftime('%a, %m %d, %Y')
-  end
-
-   def time_format
-    self.date.strftime('%H:%M')
   end
 
   def descript_limit
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_163443) do
+ActiveRecord::Schema.define(version: 2019_03_16_121536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_03_14_163443) do
     t.datetime "updated_at", null: false
     t.bigint "venue_id"
     t.string "url_link"
+    t.date "end_date"
     t.index ["venue_id"], name: "index_events_on_venue_id"
   end
 
