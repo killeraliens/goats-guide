a56a2c9dff73d3d0463ee0416d488eb5fe8beb2d
alexandra campbell <killeraliens@outlook.com>
2019-03-19 12:50:03 +0000

    submit tags for events and past

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,8 @@ class EventsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @events = Event.upcoming_events
+    @events = Event.order(date: 'ASC').upcoming_events if params["events"].present?
+    @events = Event.order(date: 'ASC').past_events if params["past"].present?
     # if params[:query].present?
     #   sql_query = " \
     #     events.title ILIKE :query \
