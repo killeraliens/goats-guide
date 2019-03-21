80669136aeaf9472923b8854f73ac0da68364eef
alexandra campbell <killeraliens@outlook.com>
2019-03-21 01:14:52 +0000

    coordinates to venues

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -3,4 +3,8 @@ class Venue < ApplicationRecord
   validates :name, :city, :country, presence: true
   validates :name, :street_address, :city, :state, :country, case_sensitive: false
   validates :name, uniqueness: { scope: :city }
+
+  def address
+    [street, city, state, country].compact.join(', ')
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_19_191309) do
+ActiveRecord::Schema.define(version: 2019_03_21_011338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,8 @@ ActiveRecord::Schema.define(version: 2019_03_19_191309) do
     t.string "state"
     t.string "country"
     t.string "info"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   add_foreign_key "events", "users"
