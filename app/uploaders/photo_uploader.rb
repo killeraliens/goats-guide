5f81b8808868a449f038112e0ccd085b882a5272
alexandra campbell <killeraliens@outlook.com>
2019-03-23 18:02:31 +0100

    default image and svg config

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,7 +1,7 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
-  def default_url
-    ActionController::Base.helpers.asset_path("default_event.png")
+  def default_url(*args)
+    ActionController::Base.helpers.asset_path("fallback/2_default.png")
   end
 end
