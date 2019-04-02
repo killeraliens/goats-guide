08994c8b610c6844f0c8667d1a1a42a755e68102
alexandra campbell <killeraliens@outlook.com>
2019-04-02 19:20:19 +0200

    url validations

--- app/models/url_link.rb
+++ app/models/url_link.rb
@@ -1,5 +1,10 @@
 class UrlLink < ApplicationRecord
   belongs_to :user
   # validates :link (on length)
-  # validates :url (http regex)
+  validates_format_of :url, with: URI::regexp(%w(http https))
+  before_validation :format_url
+
+  def format_url
+    self.url = "http://#{self.url}" unless self.url[/^https?/]
+  end
 end
