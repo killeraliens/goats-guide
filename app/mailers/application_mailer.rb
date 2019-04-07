ed7eb07ddcbb96e8a2ac4a3e2d50fce552538904
alexandra campbell <killeraliens@outlook.com>
2019-04-07 20:40:35 +0200

    mailer prep

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'no-reply@goatsguide.com'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,15 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "http://www.goatsguide.com" }
   # Settings specified here will take precedence over those in config/application.rb.
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  # address:              'smtp.gmail.com',
+  # port:                 25,
+  # domain:               'goatsguide.com',
+  # user_name:            '<username>',
+  # password:             '<password>',
+  # authentication:       'plain',
+  # enable_starttls_auto: true }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
