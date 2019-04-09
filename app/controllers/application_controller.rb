97d108b26027855ea27de711c065b3b8502ae103
alexandra campbell <killeraliens@outlook.com>
2019-04-09 12:52:50 +0200

    main container lg format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :city, :state, :country])
   end
+
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
 end
