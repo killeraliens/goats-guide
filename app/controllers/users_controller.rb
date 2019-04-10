c046a2bf633c5aee40e972661848ea7ee2a9b2e2
alexandra campbell <killeraliens@outlook.com>
2019-04-10 19:49:30 +0200

    user created event listing logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,13 @@ class UsersController < ApplicationController
     saved_event_ids = @user.saved_events.map {|event| event.event_id }
     saved_events = Event.find(saved_event_ids)
     @events = saved_events.select{|event| event.date >= Date.today}.sort_by{|event| event.date}
-    @past_saved = saved_events.select{|event| event.date < Date.today}.sort_by{|event| event.date}
+    @past_saved = saved_events.select{|event| event.end_date < Date.today}.sort_by{|event| event.end_date}
 
   end
 
   def created
     @events = Event.where(creator_id: @user).order(date: 'ASC')
+    @past_created = Event.where("creator_id = ? AND end_date < ?", @user, Date.today)
   end
 
   def edit
