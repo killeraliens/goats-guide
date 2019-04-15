06a6e5a84f271bf0cb42576b6b94c576a1823b6b
alexandra campbell <killeraliens@outlook.com>
2019-04-15 15:26:43 +0200

    muted truncate js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,15 @@
-import { ellipsizeTextBox } from '../components/truncate';
 import "bootstrap";
 import { userAutocomplete } from '../plugins/user_autocomplete';
+// import { ellipsizeTextBox } from '../components/truncate';
 
 
-ellipsizeTextBox(".card-title", ".card-title-original");
-ellipsizeTextBox(".card-location-row", ".card-location-row-original");
+// ellipsizeTextBox(".card-title", ".card-title-original");
+// ellipsizeTextBox(".card-location-row", ".card-location-row-original");
 
-window.onresize = function(event){
-    ellipsizeTextBox(".card-title", ".card-title-original");
-    ellipsizeTextBox(".card-location-row", ".card-location-row-original");
-}
+// window.onresize = function(event){
+//     ellipsizeTextBox(".card-title", ".card-title-original");
+//     ellipsizeTextBox(".card-location-row", ".card-location-row-original");
+// }
 
 userAutocomplete();
 
