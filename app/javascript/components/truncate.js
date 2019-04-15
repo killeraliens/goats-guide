32bdea49d8e2098307676359640bb9a159b6567f
alexandra campbell <killeraliens@outlook.com>
2019-04-15 18:21:16 +0200

    checking for infin loop js

--- app/javascript/components/truncate.js
+++ app/javascript/components/truncate.js
@@ -2,18 +2,23 @@ const ellipsizeTextBox = (claA, claH) => {
 
   const els = document.querySelectorAll(claA);
   const elsH = document.querySelectorAll(claH);
-  for (let i = 0; i < els.length; i++ ) {
+
+  for (let i = 0; i <= els.length; i++ ) {
     let el = els[i];
     let elH = elsH[i];
     el.innerHTML = elH.innerHTML;
     let wordArray = el.innerHTML.split(' ');
-    while(el.scrollHeight > el.offsetHeight) {
+
+    let ii=0;
+    while(el.scrollHeight >= el.offsetHeight) {
         wordArray.pop();
         el.innerHTML = wordArray.join(' ') + '...';
+        if(ii++ >= 5) break;
      }
   };
 }
 
 
 export { ellipsizeTextBox };
+
 
