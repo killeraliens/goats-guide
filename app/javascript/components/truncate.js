cd327daf07c4cc96a0293bbc7fa30399feb9671d
alexandra campbell <killeraliens@outlook.com>
2019-04-15 11:18:56 +0200

    heroku resize freeze trbleshoot

--- app/javascript/components/truncate.js
+++ app/javascript/components/truncate.js
@@ -12,7 +12,7 @@ const ellipsizeTextBox = (claA, claH) => {
   // alert('yo');
   const els = document.querySelectorAll(claA);
   const elsH = document.querySelectorAll(claH);
-  for (let i = 0; i < els.length; i++ ) {
+  for (let i = 0; i <= els.length; i++ ) {
     let el = els[i];
     let elH = elsH[i];
     el.innerHTML = elH.innerHTML;
