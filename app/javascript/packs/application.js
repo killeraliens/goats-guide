89bc48acb69434523d5db6da765c16c41f5eb9f8
alexandra campbell <killeraliens@outlook.com>
2019-04-15 16:46:12 +0200

    resize off

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,12 +6,13 @@ import { ellipsizeTextBox } from '../components/truncate';
 ellipsizeTextBox(".card-title", ".card-title-original");
 ellipsizeTextBox(".card-location-row", ".card-location-row-original");
 
-$(function() {
-window.onresize = function(event){
-    ellipsizeTextBox(".card-title", ".card-title-original");
-    ellipsizeTextBox(".card-location-row", ".card-location-row-original");
-}
-});
+
+// window.onresize = function(event){
+//     ellipsizeTextBox(".card-title", ".card-title-original");
+//     ellipsizeTextBox(".card-location-row", ".card-location-row-original");
+// }
+
+
 
 userAutocomplete();
 
