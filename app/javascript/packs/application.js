0329b88fe0d89358e8e5146a881b22b5cdfbdd4d
alexandra campbell <killeraliens@outlook.com>
2019-04-15 22:14:29 +0200

    truncate css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,22 +1,20 @@
 import "bootstrap";
 import { userAutocomplete } from '../plugins/user_autocomplete';
-import { ellipsizeTextBox } from '../components/truncate';
+// import { ellipsizeTextBox } from '../components/truncate';
 
 
-ellipsizeTextBox(".card-title", ".card-title-original");
-ellipsizeTextBox(".card-location-row", ".card-location-row-original");
+// ellipsizeTextBox(".card-title", ".card-title-original");
+// ellipsizeTextBox(".card-location-row", ".card-location-row-original");
 
-window.addEventListener('resize', function(event){
-    ellipsizeTextBox(".card-title", ".card-title-original");
-    ellipsizeTextBox(".card-location-row", ".card-location-row-original");
-});
+// window.addEventListener('resize', function(event){
+//     ellipsizeTextBox(".card-title", ".card-title-original");
+//     ellipsizeTextBox(".card-location-row", ".card-location-row-original");
+// });
 
 
 // window.onresize = function(event){
 //     ellipsizeTextBox(".card-title", ".card-title-original");
 //     ellipsizeTextBox(".card-location-row", ".card-location-row-original");
 // }
-
-
 
 userAutocomplete();
