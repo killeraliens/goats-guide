f5af4915be69d3d78ee1ce087f3cf2cd3dd95475
alexandra campbell <killeraliens@outlook.com>
2019-04-16 19:44:20 +0200

    map views

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,9 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { userAutocomplete } from '../plugins/user_autocomplete';
-import { initMapbox } from '../plugins/init_mapbox';
+// import { initMapbox } from '../plugins/init_mapbox';
 
-initMapbox();
+// initMapbox();
 userAutocomplete();
 
 
